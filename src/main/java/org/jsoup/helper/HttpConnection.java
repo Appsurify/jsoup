70a4d264e2613e7d220e84a8ff7d0e3eed049ffc
James Farrier <jamesfarrier@gmail.com>
2022-04-28 00:40:35 +1200

    Revert "mutant changes"
    This reverts commit 0c6cb00a33b400f115eee3908c09590e5d2e780b.

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -1086,7 +1086,7 @@ public class HttpConnection implements Connection {
                 sb.append(cookie.getKey()).append('=').append(cookie.getValue());
                 // todo: spec says only ascii, no escaping / encoding defined. validate on set? or escape somehow here?
             }
-            return StringUtil.releaseBuilder(sb);
+            return StringUtil.releaseBuilder(sb); 
         }
 
         // for get url reqs, serialise the data map into the url
@@ -1149,7 +1149,7 @@ public class HttpConnection implements Connection {
         public KeyVal key(String key) {
             Validate.notEmpty(key, "Data key must not be empty");
             this.key = key;
-            return this; 
+            return this;
         }
 
         public String key() {
