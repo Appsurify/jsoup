fd101fa8e8c21a6cfa520356deecbb83defadcd5
James Farrier <jamesfarrier@gmail.com>
2022-04-27 14:51:28 +1200

    mutant changes

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -508,7 +508,7 @@ public class HttpConnection implements Connection {
         }
 
         public T cookie(String name, String value) {
-            Validate.notEmpty(name, "Cookie name must not be empty"); 
+            Validate.notEmpty(name, "Cookie name must not be empty");
             Validate.notNull(value, "Cookie value must not be null");
             cookies.put(name, value);
             return (T) this;
@@ -815,7 +815,7 @@ public class HttpConnection implements Connection {
 
         public Response charset(String charset) {
             this.charset = charset;
-            return this;
+            return this; 
         }
 
         public String contentType() {
