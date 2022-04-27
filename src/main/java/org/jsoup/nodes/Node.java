2b1e16fd01f00a18a2b28521618db66c41bd67e9
James Farrier <jamesfarrier@gmail.com>
2022-04-27 12:47:44 +1200

    Revert "mutant changes"
    This reverts commit c2c16cea97836f1f8e34d429a33539c046c86922.

--- src/main/java/org/jsoup/nodes/Node.java
+++ src/main/java/org/jsoup/nodes/Node.java
@@ -266,7 +266,7 @@ public abstract class Node implements Cloneable {
      */
     public Document ownerDocument() {
         Node root = root();
-        return (root instanceof Document) ? (Document) root : null; 
+        return (root instanceof Document) ? (Document) root : null;
     }
 
     /**
@@ -713,7 +713,7 @@ public abstract class Node implements Cloneable {
         }
 
         public void tail(Node node, int depth) {
-            if (!node.nodeName().equals("#text")) { // saves a void hit.
+            if (!node.nodeName().equals("#text")) { // saves a void hit. 
 				try {
 					node.outerHtmlTail(accum, depth, out);
 				} catch (IOException exception) {
