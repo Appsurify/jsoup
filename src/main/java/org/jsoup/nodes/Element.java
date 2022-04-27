82118a83543510223a6a9dd3d9ac1fce399a11cb
James Farrier <jamesfarrier@gmail.com>
2022-04-27 19:04:14 +1200

    mutant changes

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -160,7 +160,7 @@ public class Element extends Node {
      * @return the tag object
      */
     public Tag tag() {
-        return tag;
+        return tag; 
     }
     
     /**
@@ -1294,7 +1294,7 @@ public class Element extends Node {
         // otherwise, scan for whitespace and compare regions (with no string or arraylist allocations)
         boolean inClass = false;
         int start = 0;
-        for (int i = 0; i < len; i++) { 
+        for (int i = 0; i < len; i++) {
             if (Character.isWhitespace(classAttr.charAt(i))) {
                 if (inClass) {
                     // white space ends a class name, compare it with the requested one, ignore case
