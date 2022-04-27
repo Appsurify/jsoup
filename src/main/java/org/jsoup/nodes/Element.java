c16f0f8a14361c5a13b10166d48bbea9eae2222e
James Farrier <jamesfarrier@gmail.com>
2022-04-27 20:32:25 +1200

    mutant changes

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -242,7 +242,7 @@ public class Element extends Node {
 
     private static void accumulateParents(Element el, Elements parents) {
         Element parent = el.parent();
-        if (parent != null && !parent.tagName().equals("#root")) {
+        if (parent != null && !parent.tagName().equals("#root")) { 
             parents.add(parent);
             accumulateParents(parent, parents);
         }
@@ -1294,7 +1294,7 @@ public class Element extends Node {
         // otherwise, scan for whitespace and compare regions (with no string or arraylist allocations)
         boolean inClass = false;
         int start = 0;
-        for (int i = 0; i < len; i++) { 
+        for (int i = 0; i < len; i++) {
             if (Character.isWhitespace(classAttr.charAt(i))) {
                 if (inClass) {
                     // white space ends a class name, compare it with the requested one, ignore case
