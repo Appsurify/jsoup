d8551d207f3c6157fefe7d43b729b9409c6cd54a
James Farrier <jamesfarrier@gmail.com>
2022-04-27 17:41:12 +1200

    Revert "mutant changes"
    This reverts commit 95e12b2ddcde51b64d624f2dbe20de9036148471.

--- src/main/java/org/jsoup/nodes/Document.java
+++ src/main/java/org/jsoup/nodes/Document.java
@@ -94,7 +94,7 @@ public class Document extends Element {
     public void title(String title) {
         Validate.notNull(title);
         Element titleEl = getElementsByTag("title").first();
-        if (titleEl == null) { // add to head
+        if (titleEl == null) { // add to head 
             head().appendElement("title").text(title);
         } else {
             titleEl.text(title);
@@ -195,7 +195,7 @@ public class Document extends Element {
 
     @Override
     public String outerHtml() {
-        return super.html(); // no outer wrapper tag 
+        return super.html(); // no outer wrapper tag
     }
 
     /**
