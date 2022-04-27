cdf2841506ffb533370adb17d1395b243b8f3db9
James Farrier <jamesfarrier@gmail.com>
2022-04-28 00:06:13 +1200

    mutant changes

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -755,7 +755,7 @@ public class Element extends Node {
      * @return position in element sibling list
      */
     public int elementSiblingIndex() {
-       if (parent() == null) return 0;
+       if (parent() == null) return 0; 
        return indexInList(this, parent().childElementsList());
     }
 
@@ -1294,7 +1294,7 @@ public class Element extends Node {
         // otherwise, scan for whitespace and compare regions (with no string or arraylist allocations)
         boolean inClass = false;
         int start = 0;
-        for (int i = 0; i < len; i++) { 
+        for (int i = 0; i < len; i++) {
             if (Character.isWhitespace(classAttr.charAt(i))) {
                 if (inClass) {
                     // white space ends a class name, compare it with the requested one, ignore case
