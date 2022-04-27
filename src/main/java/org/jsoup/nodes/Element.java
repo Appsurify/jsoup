c6eb7b00f1ac963ab95ca4d882794099d0f544dd
James Farrier <jamesfarrier@gmail.com>
2022-04-27 14:42:51 +1200

    mutant changes

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -293,7 +293,7 @@ public class Element extends Node {
             }
             shadowChildrenRef = new WeakReference<>(children);
         }
-        return children;
+        return children; 
     }
 
     /**
