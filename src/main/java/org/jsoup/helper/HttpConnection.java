d4b80b446ce5c5de672a64b738d44d6f4172a18f
James Farrier <jamesfarrier@gmail.com>
2022-04-27 18:46:22 +1200

    Revert "mutant changes"
    This reverts commit 346f5ff61e9ec474db5d79148f5dbe515605f4ef.

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -508,7 +508,7 @@ public class HttpConnection implements Connection {
         }
 
         public T cookie(String name, String value) {
-            Validate.notEmpty(name, "Cookie name must not be empty");
+            Validate.notEmpty(name, "Cookie name must not be empty"); 
             Validate.notNull(value, "Cookie value must not be null");
             cookies.put(name, value);
             return (T) this;
@@ -570,7 +570,7 @@ public class HttpConnection implements Connection {
         }
 
         public int timeout() {
-            return timeoutMilliseconds; 
+            return timeoutMilliseconds;
         }
 
         public Request timeout(int millis) {
