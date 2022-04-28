822bfb11acbcd3f8f8c7a9da6dcef5684a087ca9
James Farrier <jamesfarrier@gmail.com>
2022-04-28 16:52:10 +1200

    Revert "mutant changes"
    This reverts commit 0f0d254fad506d71fb403077ba58a97cf47ccc41.

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -1160,7 +1160,7 @@ public class Element extends Node {
                 if (el.tag.preserveWhitespace())
                     return true;
                 el = el.parent();
-                i++; 
+                i++;
             } while (i < 6 && el != null);
         }
         return false;
@@ -1294,7 +1294,7 @@ public class Element extends Node {
         // otherwise, scan for whitespace and compare regions (with no string or arraylist allocations)
         boolean inClass = false;
         int start = 0;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) { 
             if (Character.isWhitespace(classAttr.charAt(i))) {
                 if (inClass) {
                     // white space ends a class name, compare it with the requested one, ignore case
