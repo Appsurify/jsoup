cf20817a87089c8fe6a9cc401c5135b42d8e24a4
James Farrier <jamesfarrier@gmail.com>
2022-04-28 13:28:05 +1200

    Revert "mutant changes"
    This reverts commit 2a2474da793e5e8886c878aac52e03b7b642c7e3.

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -1294,7 +1294,7 @@ public class Element extends Node {
         // otherwise, scan for whitespace and compare regions (with no string or arraylist allocations)
         boolean inClass = false;
         int start = 0;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) { 
             if (Character.isWhitespace(classAttr.charAt(i))) {
                 if (inClass) {
                     // white space ends a class name, compare it with the requested one, ignore case
@@ -1393,7 +1393,7 @@ public class Element extends Node {
     }
 
     void outerHtmlHead(final Appendable accum, int depth, final Document.OutputSettings out) throws IOException {
-        if (out.prettyPrint() && (tag.formatAsBlock() || (parent() != null && parent().tag().formatAsBlock()) || out.outline())) { 
+        if (out.prettyPrint() && (tag.formatAsBlock() || (parent() != null && parent().tag().formatAsBlock()) || out.outline())) {
             if (accum instanceof StringBuilder) {
                 if (((StringBuilder) accum).length() > 0)
                     indent(accum, depth, out);
