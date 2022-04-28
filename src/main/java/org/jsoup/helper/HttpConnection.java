0d868144514f3485f84e5a7a331e053143e123f8
James Farrier <jamesfarrier@gmail.com>
2022-04-28 16:29:15 +1200

    Revert "mutant changes"
    This reverts commit d0d6070ca990328c4ffb837f4cca820c1568de51.

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -715,7 +715,7 @@ public class HttpConnection implements Connection {
 
             // set up the request for execution
             String mimeBoundary = null;
-            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody))
+            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody)) 
                 serialiseRequestUrl(req);
             else if (methodHasBody)
                 mimeBoundary = setOutputContentType(req);
@@ -871,7 +871,7 @@ public class HttpConnection implements Connection {
         @Override
         public Connection.Response bufferUp() {
             prepareByteData();
-            return this; 
+            return this;
         }
 
         @Override
