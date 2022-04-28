c019bf2a788af6f4d4cdf08c163f22af3929e7bd
James Farrier <jamesfarrier@gmail.com>
2022-04-28 13:31:43 +1200

    mutant changes

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -293,7 +293,7 @@ public class HttpConnection implements Connection {
 
     public Connection.Response execute() throws IOException {
         res = Response.execute(req);
-        return res;
+        return res; 
     }
 
     public Connection.Request request() {
@@ -715,7 +715,7 @@ public class HttpConnection implements Connection {
 
             // set up the request for execution
             String mimeBoundary = null;
-            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody)) 
+            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody))
                 serialiseRequestUrl(req);
             else if (methodHasBody)
                 mimeBoundary = setOutputContentType(req);
