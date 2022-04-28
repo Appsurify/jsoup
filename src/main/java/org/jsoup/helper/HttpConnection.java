f92a3baebdf845f960f3b25eac5d10d8ee8e5bb1
James Farrier <jamesfarrier@gmail.com>
2022-04-28 17:28:12 +1200

    mutant changes

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -235,7 +235,7 @@ public class HttpConnection implements Connection {
     }
 
     public Connection.KeyVal data(String key) {
-        Validate.notEmpty(key, "Data key must not be empty");
+        Validate.notEmpty(key, "Data key must not be empty"); 
         for (Connection.KeyVal keyVal : request().data()) {
             if (keyVal.key().equals(key))
                 return keyVal;
@@ -715,7 +715,7 @@ public class HttpConnection implements Connection {
 
             // set up the request for execution
             String mimeBoundary = null;
-            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody)) 
+            if (req.data().size() > 0 && (!methodHasBody || hasRequestBody))
                 serialiseRequestUrl(req);
             else if (methodHasBody)
                 mimeBoundary = setOutputContentType(req);
