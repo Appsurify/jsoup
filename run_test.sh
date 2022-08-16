bc1b5725920de78d891886b2e402b8bc4b9692f3
James Farrier <jamesfarrier@gmail.com>
2022-08-16 13:51:38 +1200

    latest

--- run_test.sh
+++ run_test.sh
@@ -2,10 +2,10 @@ echo "Starting the script"
 
 n_commit=200
 
-apiKey="MTpEbzhXQThOaW14bHVQTVdZZXNBTTVLT0xhZ00"
+apiKey="NTk6U1V5Wl9NeENlRURQSmt2cTNLOXBZc3ptSi16NGk1SXh5MjNhRGZvVkI5MA"
 #fileName="/f/professional/projects/java/appsurify/jsoup/target/surefire-reports/TEST-org.jsoup.helper.DataUtilTest.xml"
 project="jsoup"
-testsuite="Unit"
+testsuite="junit"
 importtype="junit"
 #commitId="9f2f5671674e4fdbaea3bf73e826354fb177111b"
 
@@ -34,7 +34,7 @@ do
 	    echo "call api for $fileName" >> console.log
       echo $commitId
       echo $fileName
-		  curl -X POST http://appsurify.test.appsurify.com/api/external/import/ -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H "token: $apiKey" -F "file=@$fileName" -F 'project_name'=$project -F 'test_suite_name'=$testsuite -F 'type'=$importtype -F 'commit'=$commitId
+		  curl -X POST https://testbraindemo.appsurify.com/api/external/import/ -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H "token: $apiKey" -F "file=@$fileName" -F 'project_name'=$project -F 'test_suite_name'=$testsuite -F 'type'=$importtype -F 'commit'=$commitId
 	  done	  
     done
 done
